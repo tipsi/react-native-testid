4ef8d6462d9285a005ec04dc8d5f9ed39a9a9788
cenx <cen0758@163.com>
2022-10-13 14:55:42 +0800

    Update index.js
    fix android crash, when testID is number

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Platform } from 'react-native'
 
 export default function testID(id) {
+  id = typeof id !== 'string' ? id + '' : id; // fix android crash, when id is number
   return Platform.OS === 'android' ?
     { accessible: true, accessibilityLabel: id } :
     { testID: id }
